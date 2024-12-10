e5d0eeb16f4acecd2b8bb875aa9332b1374edef1
khmute <khmute@gmail.com>
2024-12-10 22:22:36 +0900

    update liblouis table

--- Translation/brl_to_txt_dev.py
+++ Translation/brl_to_txt_dev.py
@@ -1,81 +1,139 @@
-import textwrap
-import louis
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 import json
+import os
 
-# tableList = ["en-ueb-g2.ctb"]
-# lineLength = 38
 
-# translation = louis.translate(tableList, "Hello world", 0, 0)
-# translation = louis.backTranslateString(tableList, "Hello world!", 0, 0)
-                
-# print(translation)
-
-# print(louis.translate(["unicode.dis","en-chardefs.cti"], "hello")[0])
-# print(louis.backTranslate(["unicode.dis","en-chardefs.cti"], "⠓⠑⠇⠇⠕")[0])
-# print(louis.translate(["unicode.dis","ko-g2.ctb"], "안녕하세요 hello")[0])
-
-# tableList = ["unicode.dis","ko-g2.ctb"]
-kor_tableList = ["ko-g2.ctb"]
-eng_tableList = ["unicode.dis","en-chardefs.cti"]
-
-def lang_detection(text):
-    # 한글, 영어 구분
-    result = []     # [{lang: KOR, brl: ''}, {lang: ENG, text: ''}]
+def translate(input_brl_list):
+    # WebDriver Manager를 사용하여 chromedriver 자동 설치 및 설정
+    service = Service(ChromeDriverManager().install())
     
-    lang = "KOR"
+    # ChromeOptions 객체 생성
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')  # 헤드리스 모드 설정
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     
-    for idx, char in enumerate(text):
-        if lang == "KOR":
-            if char == '⠴' and (idx == 0 or text[idx - 1] == chr(0x2800)):
-                lang = "ENG"
-            else:
-                result.append({'lang': lang, 'brl': char})
-        elif lang == "ENG":
-            if (char == '⠲' or char == '⠐') and (idx + 1 == len(text) or text[idx + 1] == chr(0x2800)):
-                result.append({'lang': lang, 'brl': char})
-                lang = "KOR"
-            else:
-                result.append({'lang': lang, 'brl': char})
-    return result
-
-def translate(lang, input_brl_list):
-    translated_text_list = []
-    tableList = kor_tableList # if lang == "KOR" else eng_tableList
+    driver = webdriver.Chrome(service=service, options=options)
+    
+    # 해당 페이지로 이동
+    driver.get("https://t.hi098123.com/braille")
+    
+    # 변환할 텍스트를 입력 (예시: 'happy')
+    try:
+        input_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "input")))
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "input")))
+    except Exception as e:
+        print(f"Error: {e}")
+        driver.quit()
+        return
+    # input_box.clear()  # 입력 필드 초기화
     
     for input_brl in input_brl_list:
-        translation = louis.backTranslateString(tableList, input_brl, 0, 0)
-        translated_text_list.append(translation)
-    return translated_text_list
-
-
-if __name__ == "__main__":
-    # with open('data/db/성북소식지04.json', 'r', encoding='utf-8') as f:
-    #     data = json.load(f)
-    # brl_list = data['correction']['brl']
-    # for brl in brl_list:
-    #     print(brl)
-    #     print(translate([brl]))
-    #     print()
-    text = "우리나라 기차에는 KTX, 새마을호, 무궁화호 등이 있다."
-    brl = "⠍⠐⠕⠉⠐⠣⠀⠈⠕⠰⠣⠝⠉⠵⠀⠴⠠⠠⠅⠞⠭⠐⠀⠠⠗⠑⠣⠮⠚⠥⠐⠀⠑⠍⠈⠍⠶⠚⠧⠚⠥⠀⠊⠪⠶⠕⠀⠕⠌⠊⠲"
-    # brl = "⠍⠐⠕⠉⠐⠣"
-    brl_list = [brl]
-    print(brl_list)
-    detected = lang_detection(brl)
-    result = ""
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "input")))
+        input_box.send_keys(input_brl)  # 변환할 텍스트 입력
+        input_box.send_keys("\n")
     
-    cur_lang = "KOR"
-    buffer = ""
-    for d in detected:
-        print(d)
-        if d['lang'] != cur_lang:
-            print(buffer)
-            result += translate(cur_lang, [buffer])[0]
-            buffer = d['brl']
-            cur_lang = d['lang']
+    # 역점역 버튼 클릭 (JavaScript 사용)
+    try:
+        convert_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[@id='bar']/button[@data-name='역점역 (점자해석/묵역)']"))
+        )
+        driver.execute_script("arguments[0].click();", convert_button)
+    except Exception as e:
+        print(f"Error: {e}")
+        driver.quit()
+        return
+    
+    # 변환된 점자 결과를 가져오기
+    try:
+        output_box = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "plain"))
+        )
+        result_text = output_box.text  # 결과 텍스트 가져오기
+        result_text_list = result_text.split("\n")
+    except Exception as e:
+        print(f"Error: {e}")
+        driver.quit()
+        return
+
+    print(f"입력 텍스트: {input_brl}\n변환된 점자: {result_text_list}")
+    
+    # 브라우저 종료
+    driver.quit()
+    
+    return result_text_list
+
+def test():
+    with open('validate_list.txt', 'r') as f:
+        ith_validate = f.readlines()
+    for i in ith_validate:
+        i = i.strip('\n')
+        print(i)
+        with open(i, "r", encoding="utf-8") as f:
+            json_data = json.load(f)
         
-        buffer += d['brl']
-    if buffer != "":
-        result += translate(cur_lang, [buffer])[0]
-    print(result)
+        try:
+            boxes_list = json_data['boxes']
+            brl_list = json_data['brl']
+            # labels_list = json_data['labels']
+        except:
+            # boxes_list = json_data['prediction']['boxes']
+            # brl_list = json_data['prediction']['brl']
+            # labels_list = json_data['prediction']['labels']
+            continue
         
+        translated_text_list = translate(brl_list)
+        result_data = {}
+        result_data['prediction'] = {}
+        result_data['prediction']['boxes'] = boxes_list
+        result_data['prediction']['brl'] = brl_list
+        # result_data['prediction']['labels'] = labels_list if 'labels' in json_data else []
+        result_data['prediction']['text'] = translated_text_list
+        
+        result_data['correction'] = {}
+        result_data['correction']['boxes'] = []
+        result_data['correction']['brl'] = []
+        result_data['correction']['labels'] = []
+        result_data['correction']['text'] = []
+        
+        with open(i, "w", encoding="utf-8") as f:
+            json.dump(result_data, f, ensure_ascii=False, indent=4)
+
+if __name__ == '__main__':
+    # test()
+    import time
+    cur_time = time.time()
+    print(cur_time)
+    translate([
+            "⠼⠁⠀⠦⠆⠼⠁⠰⠴⠑⠛⠊⠒⠝⠠⠎⠉⠵⠀⠟⠐⠩⠀⠱⠁⠇⠝⠠⠎",
+            "⠫⠰⠕⠫⠀⠑⠗⠍⠀⠋⠵⠀⠕⠨⠕⠃⠓⠪⠀⠇⠶⠚⠻⠀⠑⠛⠨⠐⠮⠐",
+            "⠦⠆⠼⠃⠰⠴⠑⠛⠊⠒⠝⠠⠎⠉⠵⠀⠟⠐⠩⠀⠰⠽⠰⠥⠺⠀⠇⠶⠚⠻",
+            "⠑⠛⠨⠣⠟⠀⠈⠥⠊⠗⠀⠕⠨⠕⠃⠓⠪⠀⠇⠶⠚⠻⠀⠑⠛⠨⠐⠮⠐",
+            "⠦⠆⠼⠉⠰⠴⠑⠛⠊⠒⠝⠠⠎⠉⠵⠀⠈⠥⠊⠗⠀⠕⠨⠕⠃⠓⠪⠀⠇⠶⠚⠻",
+            "⠑⠛⠨⠣⠺⠀⠚⠗⠊⠭⠀⠈⠌⠈⠕⠫⠀⠊⠽⠒⠀⠠⠦⠐⠥⠨⠝⠓⠠⠹⠴⠄",
+            "⠮⠐⠀⠦⠆⠼⠙⠰⠴⠑⠛⠊⠒⠝⠠⠎⠉⠵⠀⠑⠛⠨⠣⠺⠀⠘⠂⠊⠂",
+            "⠈⠧⠨⠻⠮⠀⠘⠥⠱⠀⠨⠍⠉⠵⠀⠈⠥⠊⠗⠀⠕⠨⠕⠃⠓⠪⠀⠇⠶⠚⠻",
+            "⠑⠛⠨⠣⠝⠀⠊⠗⠚⠗⠀⠠⠞⠑⠻⠚⠈⠥⠀⠕⠌⠠⠪⠃⠉⠕⠊⠲",
+            "⠼⠃⠀⠈⠥⠊⠗⠀⠕⠨⠕⠃⠓⠪⠀⠇⠶⠚⠻⠀⠑⠛⠨⠣⠟",
+            "⠠⠦⠚⠕⠝⠐⠥⠈⠮⠐⠕⠙⠪⠴⠄⠉⠵⠀⠰⠥⠈⠕⠝⠉⠵",
+            "⠠⠊⠪⠄⠈⠮⠨⠣⠧⠀⠠⠥⠐⠕⠈⠮⠨⠣⠺⠀⠠⠻⠈⠱⠁⠕",
+            "⠚⠷⠬⠶⠊⠽⠊⠫⠀⠨⠎⠢⠰⠣⠀⠠⠥⠐⠕⠐⠮⠀⠉⠓⠉⠗⠉⠵",
+            "⠑⠛⠨⠐⠥⠀⠘⠠⠈⠍⠗⠎⠌⠉⠵⠊⠝⠐⠀⠕⠀⠈⠧⠨⠻⠕⠀⠑⠛⠨⠣⠺",
+            "⠕⠂⠘⠒⠨⠹⠟⠀⠘⠂⠊⠂⠀⠈⠧⠨⠻⠮⠀⠘⠥⠱⠀⠨⠛⠊⠉⠵",
+            "⠨⠎⠢⠝⠠⠎⠀⠋⠵⠀⠫⠰⠕⠫⠀⠕⠌⠠⠪⠃⠉⠕⠊⠲",
+            "⠿⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠿",
+            "⠼⠚⠋⠉⠠⠨⠭⠀⠥⠉⠮⠺⠀⠎⠚⠍⠗",
+            "⠕⠂⠇⠶⠐⠂⠀⠉⠂⠑⠊⠀⠘⠒⠘⠭⠊⠽⠉⠵⠀⠠⠗⠶⠚⠧⠂⠲",
+            "⠘⠱⠁⠚⠧⠐⠂⠀⠈⠾⠑⠯⠕⠉⠀⠊⠿⠈⠯⠐⠀⠑⠍⠊⠎⠢",
+            "⠊⠪⠶⠺⠀⠘⠱⠁⠝⠀⠈⠪⠐⠟⠀⠈⠪⠐⠕⠢⠲",
+            "⠑⠛⠜⠶⠐⠂⠀⠨⠍⠐⠥⠀⠈⠾⠑⠯⠕⠉⠀⠈⠿⠌⠙⠍⠢⠀⠊⠪⠶⠺",
+            "⠑⠍⠉⠺⠲",
+            "⠙⠼⠁⠉⠀⠀⠑⠛⠚⠧⠀⠼⠚⠁⠀⠑"
+        ])
+    print(time.time() - cur_time)
